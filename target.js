16d79bafa944af6059b8d966172853bb1d4efe40
syzygy333 <brianjonashogue@gmail.com>
2015-11-03 11:15:07 -0500

    add css, flesh out html, add js logic

--- target.js
+++ target.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
+  var position = $('#target').className;
   $('#target').mouseover(function() {
-    // make target move
-  })
-})
+    switch(position) {
+      case 'top-left':
+        $('#target').className = "top-right";
+        break;
+      case 'top-right':
+        $('#target').className = "bottom-right";
+        break;
+      case 'bottom-right':
+        $('#target').className = "bottom-left";
+        break;
+      case 'bottom-left':
+        $('#target').className = "top-left";
+        break;
+    };
+  });
+});
